3809a2f7af1deb02fffa57b5304ca77c53aa2db3
Your Name <you@example.com>
2020-08-31 10:45:47 -0300

    AddSecurityDefinition

--- 1_API/TDA.WebApi/InfraEstructure/SwaggerServiceExtensions.cs
+++ 1_API/TDA.WebApi/InfraEstructure/SwaggerServiceExtensions.cs
@@ -15,6 +15,13 @@ namespace TDA.WebApi.InfraEstructure
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1.0", new OpenApiInfo { Title = "TDA API v1.0", Version = "v1.0" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
             });
             return services;
         }
